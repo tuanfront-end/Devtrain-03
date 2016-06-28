4821df6b62145445acfa00550afdd00483f4d0a6
eden_tuan <atuanvptd@gmail.com>
2016-06-28 15:39:29 +0700

    Update-js/js

--- js/js.js
+++ js/js.js
@@ -1,35 +1,54 @@
 
     $(document).ready(function() {
- 
-      var owl = $("#owl-demo");
-     
-      owl.owlCarousel({
-          items : 4, //10 items above 1000px browser width
-          itemsDesktop : [1000,4], //5 items between 1000px and 901px
-          itemsDesktopSmall : [900,3], // betweem 900px and 601px
-          itemsTablet: [600,2], //2 items between 600 and 0
-          itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-          pagination : false
-      });
-     
-      // Custom Navigation Events
-      $(".next").click(function(){
-        owl.trigger('owl.next');
-      })
-      $(".prev").click(function(){
-        owl.trigger('owl.prev');
-      })
-      // End owlCarousel_________
-     
-      //----------------------------------------------------------------------
-      // external js: isotope.pkgd.js
+      //OWLCarolsel-Js-------------------------------------------------------
+        var owl = $("#owl-demo");
+        owl.owlCarousel({
+            items : 4, //10 items above 1000px browser width
+            itemsDesktop : [1000,4], //5 items between 1000px and 901px
+            itemsDesktopSmall : [900,3], // betweem 900px and 601px
+            itemsTablet: [600,2], //2 items between 600 and 0
+            itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+            pagination : false
+        });
 
+        $(".next").click(function(){
+          owl.trigger('owl.next');
+        })
+        $(".prev").click(function(){
+          owl.trigger('owl.prev');
+        })
+        // End owlCarousel_________-------------------------------------------------------
+        
+
+        // JS- Validate -Form-------------------------------------------------------
+         $('#contact-form').validate({ 
+           rules: {
+               email: {
+                   required: true,
+                   email: true
+               },
+               name: {
+                   required: true,
+                   minlength: 3
+               },
+               Subject: {
+                   required: true,
+                   number: true,
+                   minlength: 5
+               },
+               Description: {
+                   required: true,
+                   minlength: 10
+               }
+           }
+        });
+       // JS- Validate -Form-------------------------------------------------------
+   
+      // external js: isotope.pkgd.js-------------------------------------------------------
         $(".grid").isotope({
            itemSelector: '.grid-item-2',
            layoutMode: 'fitRows',
         });
-
-         // Add isotope click funtion
 
         $('.portfolio-list li').click(function(){
           $('.portfolio-list li').removeClass('active');
@@ -47,90 +66,66 @@
             return false;
         });
 
-        // external js: isotope.pkgd.js
-
-
       //- END ISOTOPE---------------------------------------------------------------------
 
-      //- Light box---------------------------------------------------------------------
-       lightbox.option({
+        //- Light box---------------------------------------------------------------------
+         lightbox.option({
             'resizeDuration': 200,
             'wrapAround': true
           })
          //- end Light box---------------------------------------------------------------------
-     
-    });
 
 
 
 
-    $(document).ready(function(){
+        //  REnponsive-menu________----------------------------------------------------------
         var animationNameIn = 'animated bounceInLeft';
-        var animationNameOut = 'animated hinge';
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+          var animationNameOut = 'animated hinge';
+          var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-       $('.open-nav').click(function(){
-        
-          $(".reponsive-nav-list").css({"z-index":"100","width":"100%","display":"block","font-size":"40px",
-            "text-align":"center"});
-          $(".reponsive-nav-list").addClass(animationNameIn).one(animationEnd,function(){
-            $(this).removeClass(animationNameIn);
-         });
-      });
-        $('.close-nav').click(function(){
-           $(".reponsive-nav-list").addClass(animationNameOut).one(animationEnd,function(){
-             $(this).css("display","none");
-             $(this).removeClass(animationNameOut);
+         $('.open-nav').click(function(){
+          
+              $(".reponsive-nav-list").css({"z-index":"100","width":"100%","display":"block","font-size":"40px",
+                "text-align":"center"});
+              $(".reponsive-nav-list").addClass(animationNameIn).one(animationEnd,function(){
+                $(this).removeClass(animationNameIn);
+             });
           });
-       });
+          $('.close-nav').click(function(){
+             $(".reponsive-nav-list").addClass(animationNameOut).one(animationEnd,function(){
+               $(this).css("display","none");
+               $(this).removeClass(animationNameOut);
+            });
+         });
+        // End REnponsive-menu________----------------------------------------------------------
+
+
+        // Flex slide2________----------------------------------------------------------
+         $(window).load(function() {
+            $('.flexslider').flexslider({
+              animation: "slide"
+            });
+          });
+         //end  Flex slide2________----------------------------------------------------------
+
+          // Jquery for Class Naviar-sub-----------------------------------------------------------
+          $("ul.my-nav li").has("ul").addClass("sub-menu");
+          $("li.sub-menu").append("<i class='fa fa-chevron-down' aria-hidden='true'></i>");
+
+          //End Jquery for Class Naviar-sub-----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
     });
- // End REnponsive-menu________
-
-
- //Flex Slider-2-________
-   $(document).ready(function(){
-      $(window).load(function() {
-        $('.flexslider').flexslider({
-          animation: "slide"
-        });
-      });
-   });
-      
- // End Flex Slider-2-________
-
-
-
-    // Jquery for Class Naviar-sub-________
-    $(document).ready(function(){
-      $("ul.my-nav li").has("ul").addClass("sub-menu");
-      $("li.sub-menu").append("<i class='fa fa-chevron-down' aria-hidden='true'></i>");
-      // $("li.sub-menu").children("a").addClass("a-plus");
-      // $("a.a-plus").append("<i class='fa fa-chevron-down' aria-hidden='true'></i>");
-    });
-    // EndJquery for Class Naviar-sub-________
-
-
-$(document).ready(function () {
-   $('#contact-form').validate({ 
-         rules: {
-             email: {
-                 required: true,
-                 email: true
-             },
-             name: {
-                 required: true,
-                 minlength: 3
-             },
-             Subject: {
-                 required: true,
-                 number: true,
-                 minlength: 5
-             },
-             Description: {
-                 required: true,
-                 minlength: 10
-             }
-         }
-     });
- 
-});
